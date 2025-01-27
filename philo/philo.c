11da01b7c74b98f9d3ff2ce026d1dd11648b7299
Vanessa Rodrigues <varodrig@paul-f4Br9s2.clusters.42paris.fr>
2025-01-27 18:58:22 +0100

    start

---
+++ philo/philo.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: varodrig <varodrig@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/27 17:53:23 by varodrig          #+#    #+#             */
+/*   Updated: 2025/01/27 18:07:37 by varodrig         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+philo >= 1
+
+philo's behaviour:
+- eat
+- think
+- sleep
+
+forks = philo
+
+eat(2 forks)
+
+if 1 philo dies, exit
+
+arguments :
+- nb of philo
+- time to die (eat-eat or start-eat) (ms)
+- time to eat (between mutex ?)(ms)
+- time to sleep (ms) usleep(time to sleep)
+- optional 
+*/
