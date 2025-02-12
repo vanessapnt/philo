38ef734703f13865b4dabb9393cd7cca7f134f75
vanessapnt <vanessardgspnt@gmail.com>
2025-02-12 18:13:24 +0100

    init

--- philo/redlight.c
+++ philo/redlight.c
@@ -1,6 +1,30 @@
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <pthread.h>
+#include <bools.h>
+
+typedef struct s_philo
+{
+	int	number_of_philosophers;
+	int time_to_die;
+	int time_to_eat;
+	int time_to_sleep;
+	int number_of_times_each_philosopher_must_eat;
+	int id;
+	pthread_mutex_t	left_fork;
+	pthread_mutex_t	right_fork;
+	pthread_mutex_t dead;
+	pthread_mutex_t stomach;
+	int eaten_times;
+}	t_philo;
+
+typedef struct s_waiter
+{
+	bool dead_flag;
+	pthread_mutex_t	dead_mutex;
+}	t_waiter;
 
 int	ft_isdigit(char c) ///TO CHANGE TO LIBFT
 {
@@ -27,7 +51,7 @@ typedef enum e_error
 void	ft_err(t_error error)
 {
 	if (error == NB_PHILO)
-		printf("Too many philosophers. Try with max 20 philosophers\n");
+		printf("Too many philosophers. Try with max 200 philosophers\n");
 	else if (error == VALUES_TOO_LOW)
 		printf("The values must be integers and greater than 0.\n");
 	else if (error == NB_ARGS)
@@ -39,10 +63,7 @@ void	ft_err(t_error error)
 	exit(EXIT_FAILURE);
 }
 
-#include <limits.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <errno.h>
+
 
 long ft_atol(const char *nptr)
 {
@@ -143,8 +164,73 @@ void    parsing(int argc, char **argv)
         ft_err(NB_PHILO);
 }
 
+void init_philos(char **argv, t_philo *philos)
+{
+	int i;
+
+	i = 1
+	while (i <= argv[1])
+	{
+		philos[i].number_of_philosophers = ft_atoi(argv[1]);
+		philos[i].time_to_die = ft_atoi(argv[2]);
+		philos[i].time_to_eat = ft_atoi(argv[3]);
+		philos[i].time_to_sleep = ft_atoi(argv[4]);
+		if (argv[5])
+			philos[i].num_times_to_eat = ft_atoi(argv[5]);
+		else
+			philos[i].num_times_to_eat = -1;
+		philos[i].id = i;
+		philos[i].eaten_times = 0;
+		philos[i].left_fork = &forks[i];
+		philos[i].right_fork = &forks[i % philo->number_of_philosophers + 1];
+		philos[i].dead = &waiter->dead_flag;
+		i++;
+	}
+}
+
+void init_forks(pthread_mutex_t	*forks, int philo_nbr)
+{
+	int i;
+
+	i = 1;
+	while(i <= philo_nbr)
+	{
+		pthread_mutex_init(&forks[i], NULL);
+		i++;
+	}
+}
+
+void init_waiter()
+{
+	waiter.dead_flag = false;
+}
+
 int main(int argc, char **argv)
 {
-    parsing(argc, argv);
+	t_philo			philos[PHILO_MAX];
+	t_waiter		waiter;
+	pthread_mutex_t	forks[PHILO_MAX];
+
+	parsing(argc, argv);
+	init_waiter();
+	init_forks(forks, ft_atoi(argv[1]));
+	init_philos(argv, &philos, &waiter);
+	dinner(&philos);
+
+	int	i;
+
+	i = 0;
+	while (int i = 0; i < 10; i++)
+	{
+		if (pthread_create(&th[i], NULL, &routine, NULL) != 0)
+			perror("Failed to create thread");
+	}
+	while (int i = 0; i < 10; i++)
+	{
+	    if (pthread_join(th[i], NULL) != 0)
+	        perror("Failed to join thread");
+	}
+
+	cleaning_dinner();
     return (0);
 }
