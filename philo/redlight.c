7c41a9885fd4e96983e8c4d64cd9bfaeee934f1c
vanessapnt <vanessardgspnt@gmail.com>
2025-02-15 15:43:18 +0100

    threads created

--- philo/redlight.c
+++ philo/redlight.c
@@ -3,18 +3,19 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <pthread.h>
-#include <bools.h>
+#include <stdbool.h>
+#define PHILO_MAX 200
 
 typedef struct s_philo
 {
-	int	number_of_philosophers;
+	pthread_t	philo_thread;
 	int time_to_die;
 	int time_to_eat;
 	int time_to_sleep;
 	int number_of_times_each_philosopher_must_eat;
 	int id;
-	pthread_mutex_t	left_fork;
-	pthread_mutex_t	right_fork;
+	pthread_mutex_t	*left_fork;
+	pthread_mutex_t	*right_fork;
 	pthread_mutex_t dead;
 	pthread_mutex_t stomach;
 	int eaten_times;
@@ -22,6 +23,8 @@ typedef struct s_philo
 
 typedef struct s_waiter
 {
+	pthread_t	waiter_thread;
+	int	number_of_philosophers;
 	bool dead_flag;
 	pthread_mutex_t	dead_mutex;
 }	t_waiter;
@@ -67,20 +70,20 @@ void	ft_err(t_error error)
 
 long ft_atol(const char *nptr)
 {
-    int i = 0;
-    long num = 0;
+	int i = 0;
+	long num = 0;
 
-    while (ft_isblank(nptr[i]) == 1 || nptr[i] == '+')
-        i++;
+	while (ft_isblank(nptr[i]) == 1 || nptr[i] == '+')
+		i++;
 
-    while (nptr[i] >= '0' && nptr[i] <= '9')
-    {
-        if (num > LONG_MAX / 10 || (num == LONG_MAX / 10 && (nptr[i] - '0') > 7))
-            ft_err(LIMIT_INT_MAX);
-        num = num * 10 + (nptr[i] - '0');
-        i++;
-    }
-    return (num);
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		if (num > LONG_MAX / 10 || (num == LONG_MAX / 10 && (nptr[i] - '0') > 7))
+			ft_err(LIMIT_INT_MAX);
+		num = num * 10 + (nptr[i] - '0');
+		i++;
+	}
+	return (num);
 }
 
 
@@ -124,66 +127,65 @@ int	ft_is_negative(char *str)
 
 int ft_only_digits(char *str)
 {
-    if (!str || *str == '\0')
+	if (!str || *str == '\0')
 		return (1);
-    int i;
-    i = 0;
-    while (ft_isblank(str[i]) == 1)
+	int i;
+	i = 0;
+	while (ft_isblank(str[i]) == 1)
 		i++;
-    if (str[i] == '+' || str[i] == '-')
-        i++;
-    while(str[i])
-    {
-        if (!ft_isdigit(str[i]))
-            return (1);
-        i++;
-    }
-    return (0);
+	if (str[i] == '+' || str[i] == '-')
+		i++;
+	while(str[i])
+	{
+		if (!ft_isdigit(str[i]))
+			return (1);
+		i++;
+	}
+	return (0);
 }
 
 void    parsing(int argc, char **argv)
 {
-    int i;
-    if (argc != 5 && argc != 6)
-        ft_err(NB_ARGS);
-    i = 0;
-    while (argv[++i])
-    {
-        if (ft_only_digits(argv[i]))
-            ft_err(DIGITS_ARGS);
+	int i;
+	if (argc != 5 && argc != 6)
+		ft_err(NB_ARGS);
+	i = 0;
+	while (argv[++i])
+	{
+		if (ft_only_digits(argv[i]))
+			ft_err(DIGITS_ARGS);
 	}
 	i = 0;
-    while (argv[++i])
-    {
-        if (ft_is_negative(argv[i]))
-            ft_err(VALUES_TOO_LOW);
-        if (ft_atol(argv[i]) > INT_MAX)
-            ft_err(LIMIT_INT_MAX);
-    }
-    if (ft_atoi(argv[1]) > 200)
-        ft_err(NB_PHILO);
+	while (argv[++i])
+	{
+		if (ft_is_negative(argv[i]))
+			ft_err(VALUES_TOO_LOW);
+		if (ft_atol(argv[i]) > INT_MAX)
+			ft_err(LIMIT_INT_MAX);
+	}
+	if (ft_atoi(argv[1]) > 200)
+		ft_err(NB_PHILO);
 }
 
-void init_philos(char **argv, t_philo *philos)
+void init_philos(char **argv, t_philo *philos, t_waiter *waiter, pthread_mutex_t	*forks)
 {
 	int i;
 
-	i = 1
-	while (i <= argv[1])
+	i = 1;
+	while (i <= waiter->number_of_philosophers)
 	{
-		philos[i].number_of_philosophers = ft_atoi(argv[1]);
 		philos[i].time_to_die = ft_atoi(argv[2]);
 		philos[i].time_to_eat = ft_atoi(argv[3]);
 		philos[i].time_to_sleep = ft_atoi(argv[4]);
 		if (argv[5])
-			philos[i].num_times_to_eat = ft_atoi(argv[5]);
+			philos[i].time_to_eat = ft_atoi(argv[5]);
 		else
-			philos[i].num_times_to_eat = -1;
+			philos[i].time_to_eat = -1;
 		philos[i].id = i;
 		philos[i].eaten_times = 0;
 		philos[i].left_fork = &forks[i];
-		philos[i].right_fork = &forks[i % philo->number_of_philosophers + 1];
-		philos[i].dead = &waiter->dead_flag;
+		philos[i].right_fork = &forks[i % waiter->number_of_philosophers + 1];
+		//philos[i].dead = &waiter->dead_flag;
 		i++;
 	}
 }
@@ -200,37 +202,76 @@ void init_forks(pthread_mutex_t	*forks, int philo_nbr)
 	}
 }
 
-void init_waiter()
+void	init_waiter(t_waiter *waiter, int philo_nbr)
 {
-	waiter.dead_flag = false;
+	waiter->number_of_philosophers = philo_nbr;
+	waiter->dead_flag = false;
 }
 
-int main(int argc, char **argv)
+void *waiter_routine(void *arg)
+{
+	t_waiter *waiter = (t_waiter *)arg;
+	printf("Hello from waiter\n");
+	return (NULL);
+}
+
+void *philo_routine(void *arg)
+{
+	t_philo *philos = (t_philo *)arg;
+	printf("Hello from %d philo\n", philos->id);
+	return (NULL);
+}
+
+void	dinner(t_philo *philos, t_waiter *waiter)
+{
+	int	i;
+	if (pthread_create(&waiter->waiter_thread, NULL, &waiter_routine, waiter) != 0)
+	{
+		perror("Failed to create thread");
+		exit(1);
+		//cleaning_dinner();
+	}	
+	i = 0;
+	while (i < waiter->number_of_philosophers)
+	{
+		if (pthread_create(&philos[i].philo_thread, NULL, &philo_routine, &philos[i]) != 0)
+		{
+			perror("Failed to create thread");
+			exit(1);
+			//cleaning_dinner();
+		}	
+		i++;
+	}
+} 
+
+int	main(int argc, char **argv)
 {
 	t_philo			philos[PHILO_MAX];
 	t_waiter		waiter;
 	pthread_mutex_t	forks[PHILO_MAX];
+	int i;
 
 	parsing(argc, argv);
-	init_waiter();
-	init_forks(forks, ft_atoi(argv[1]));
-	init_philos(argv, &philos, &waiter);
-	dinner(&philos);
+	init_waiter(&waiter, ft_atoi(argv[1]));
+	init_forks(forks, waiter.number_of_philosophers);
+	init_philos(argv, philos, &waiter, forks);
+	dinner(philos, &waiter);
 
-	int	i;
-
+	if (pthread_join(waiter.waiter_thread, NULL) != 0)
+	{
+		perror("Failed to create thread");
+		//cleaning_dinner();
+	}
 	i = 0;
-	while (int i = 0; i < 10; i++)
+	while (i < waiter.number_of_philosophers)
 	{
-		if (pthread_create(&th[i], NULL, &routine, NULL) != 0)
+		if (pthread_join(philos[i].philo_thread, NULL) != 0)
+		{
 			perror("Failed to create thread");
+			//cleaning_dinner();
+		}	
+		i++;
 	}
-	while (int i = 0; i < 10; i++)
-	{
-	    if (pthread_join(th[i], NULL) != 0)
-	        perror("Failed to join thread");
-	}
-
-	cleaning_dinner();
-    return (0);
+	//cleaning_dinner();
+	return (0);
 }
